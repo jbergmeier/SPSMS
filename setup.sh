999a757677cf8cf03484a8d93d6a37283f270d55
jbergmeier <jb@jbergmeier.com>
2020-05-12 18:35:11 +0200

    Initial Upload

---
+++ setup.sh
@@ -0,0 +1,3 @@
+TEST=TESTKEY
+DATABASE_URL=postgres://kghwovzmjnvyan:7ac092e3bdfb26f05020507056e4f2070fafa50c9dcfa1b4b4184331e8d4954b@ec2-79-125-26-232.eu-west-1.compute.amazonaws.com:5432/d72acn4sfmq0cl
+EXCITED=false
