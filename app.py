1816e88c73d41e056af62a6d158bd8c85fee2613
jbergmeier <jb@jbergmeier.com>
2020-05-13 18:22:21 +0200

    added Class App_User and first Endpoints

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from models import setup_db
-from flask import Flask
+from flask import Flask, request, abort, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from sqlalchemy.sql import exists
+from flask_migrate import Migrate, MigrateCommand
+import logging
 import os
-
-# print(os.environ['DATABASE_URL'])
+from models import setup_db, App_User
+from auth import AuthError
 
 
 def create_app(test_config=None):
@@ -12,18 +15,122 @@ def create_app(test_config=None):
     setup_db(app)
     CORS(app)
 
-    @app.route('/')
-    def get_greeting():
-        excited = os.environ['EXCITED']
-        greeting = "Hello"
-        if excited == 'true':
-            greeting = greeting + "!!!!!"
-        return greeting
+    @app.route('/createUser', methods=['POST'])
+    def create_user():
+        req = request.get_json()
+        req_email = req.get('email')
+        req_firstname = req.get('firstname')
+        req_lastname = req.get('lastname')
+        req_company = req.get('company')
+        req_address = req.get('address')
+        req_postalcode = req.get('postalcode')
+        req_postalplace = req.get('postalplace')
+        req_country = req.get('country')
 
-    @app.route('/coolkids')
-    def be_cool():
-        return "Be cool, man, be coooool! You're almost a FSND grad!"
+        new_user = App_User(firstname=req_firstname, lastname=req_lastname, email=req_email, company=req_company, address=req_address,
+                            postalcode=req_postalcode, postalplace=req_postalplace, country=req_country)
+        print(new_user)
+        new_user.insert()
 
+        return jsonify({
+            "success": True,
+            "App_User": {
+                "firstname": req_firstname,
+                "lastname": req_lastname,
+                "email": req_email
+            }
+        })
+
+    @app.route('/users', methods=['GET'])
+    def get_users():
+        all_users = App_User.query.all()
+        users = [user.short() for user in all_users]
+
+        return jsonify({
+            "success": True,
+            "users": users
+        })
+
+    @app.route('/users/<int:id>', methods=['GET'])
+    def get_single_user(id):
+        single_user = App_User.query.filter(App_User.id == id).all()
+        if not single_user:
+            abort(404)
+        try:
+            user_result = [user.short() for user in single_user]
+            return jsonify({
+                "success": True,
+                "user": user_result
+            })
+        except:
+            abort(422)
+
+    '''
+    Errorhandler for different Cases
+    '''
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            "success": False,
+            "error": 400,
+            "message": "Bad Request"
+        }), 400
+
+    @app.errorhandler(401)
+    def unauthorized(error):
+        return jsonify({
+            "success": False,
+            "error": 401,
+            "message": "Unauthorized"
+        }), 401
+
+    @app.errorhandler(403)
+    def forbidden(error):
+        return jsonify({
+            "success": False,
+            "error": 403,
+            "message": "Access Forbidden"
+        }), 403
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "Resource not found"
+        }), 404
+
+    @app.errorhandler(405)
+    def method_not_allowed(error):
+        return jsonify({
+            "success": False,
+            "error": 405,
+            "message": "The method is not allowed for the requested URL!"
+        }), 405
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "Unprocessable Entity"
+        }), 422
+
+    @app.errorhandler(500)
+    def serverError(error):
+        return jsonify({
+            "success": False,
+            "error": 500,
+            "message": "Internal Server Error"
+        }), 500
+
+    @app.errorhandler(AuthError)
+    def auth_error(e):
+        return jsonify({
+            "success": False,
+            "error": e.status_code,
+            "message": e.error
+        }), e.status_code
     return app
 
 
--- models.py
+++ models.py
@@ -1,6 +1,8 @@
-from sqlalchemy import Column, String, Integer, create_engine
+from sqlalchemy import Column, String, Integer, create_engine, DateTime
 from flask_sqlalchemy import SQLAlchemy
 import json
+from flask_migrate import Migrate, MigrateCommand
+import datetime
 import os
 
 database_path = os.environ['DATABASE_URL']
@@ -22,24 +24,66 @@ def setup_db(app, database_path=database_path):
 
 
 '''
-Person
-Have title and release year
+Users of the Database incl. Customers
 '''
 
 
-class Person(db.Model):
-    __tablename__ = 'People'
+class App_User(db.Model):
+    __tablename__ = 'app_user'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    catchphrase = Column(String)
+    firstname = Column(String, nullable=False)
+    lastname = Column(String, nullable=False)
+    company = Column(String)
+    email = Column(String, nullable=False)
+    address = Column(String)
+    postalcode = Column(String)
+    postalplace = Column(String)
+    country = Column(String)
+    created_at = Column(DateTime, default=datetime.datetime.now())
+    updated_at = Column(DateTime)
 
-    def __init__(self, name, catchphrase=""):
-        self.name = name
-        self.catchphrase = catchphrase
+    def __init__(self, firstname, lastname, company, email, address, postalcode, postalplace, country):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.company = company
+        self.email = email
+        self.address = address
+        self.postalcode = postalcode
+        self.postalplace = postalplace
+        self.country = country
 
-    def format(self):
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def short(self):
         return {
             'id': self.id,
-            'name': self.name,
-            'catchphrase': self.catchphrase}
+            'firstname': self.firstname,
+            'lastname': self.lastname,
+            'email': self.email
+        }
+
+    def long(self):
+        return {
+            'id': self.id,
+            'firstname': self.firstname,
+            'lastname': self.lastname,
+            'company': self.company,
+            'email': self.email,
+            'address': self.address,
+            'postalcode': self.postalcode,
+            'postalplace': self.postalplace,
+            'country': self.country
+        }
+
+    def __repr__(self):
+        return json.dumps(self.short())
